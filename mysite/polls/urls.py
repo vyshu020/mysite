4a182ef01cb9fa7838c98c4997410fbb03e18e41
vyshu020 <vyshu020@gmail.com>
2015-12-29 19:10:07 -0600

    Added about us

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
     url(r'^about_us', views.about_us,),
+    url(r'^$', views.index, name='index'),
+
 ]
