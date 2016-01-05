10fb66c468a51bc7cb5573a42373bfb8dfa68af4
vyshu020 <vyshu020@gmail.com>
2016-01-04 20:23:43 -0600

    imported datetime

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,5 +1,7 @@
+import datetime
 
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=600)
@@ -7,6 +9,9 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete = models.CASCADE)
