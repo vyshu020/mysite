fb9bb0c91dd42b9c0ce1a54a17467f031a17dae6
vyshu020 <vyshu020@gmail.com>
2016-01-08 18:57:25 -0600

    Added details results and votes views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     url(r'^about_us', views.about_us,),
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<question_id>[0-9]+)/$',views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/results/$',views.results, name='results'),
+    url(r'^(>P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,5 +5,16 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
 def about_us(request):
     return HttpResponse("I have created this site. My name is Vyshnavi.")
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s." %question_id)
+
+def results(request, question_id):
+    response="You are looking at the results of question %s."
+    return HttpResponse(response % qusetion_id)
+
+def vote(request, question_id):
+    return HttpResponse("you are voting on question %s."%question_id)
