50e26fff275e3e05a883da9e33f45397a3763bdd
vyshu020 <vyshu020@gmail.com>
2016-01-08 19:03:45 -0600

    Added details results and votes views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -14,7 +14,7 @@ def detail(request, question_id):
 
 def results(request, question_id):
     response="You are looking at the results of question %s."
-    return HttpResponse(response % qusetion_id)
+    return HttpResponse(response % question_id)
 
 def vote(request, question_id):
     return HttpResponse("you are voting on question %s."%question_id)
