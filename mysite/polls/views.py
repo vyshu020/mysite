478e67393750c968c87e69e974c97006384afbc7
vyshu020 <vyshu020@gmail.com>
2016-01-14 18:55:36 -0600

    added doesnot exist exception

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 
 from django.shortcuts import render
 from models import Question, Choice
+from django.http import HttpResponse,Http404
 
 # Create your views here.
 
@@ -15,7 +16,12 @@ def about_us(request):
     return HttpResponse("I have created this site. My name is Vyshnavi.")
 
 def detail(request, question_id):
-    return HttpResponse("You are looking at question %s." %question_id)
+    try:
+        question=Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    context ={'question':question}
+    return render(request,'polls/details.html',context)
 
 def results(request, question_id):
     response="You are looking at the results of question %s."
