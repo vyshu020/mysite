f9f721afd9bda08ca8c79722038b3a3a2c35e953
vyshu020 <vyshu020@gmail.com>
2016-01-20 18:10:46 -0600

    added code to count votes in db

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,8 @@
 
 from django.shortcuts import render, get_object_or_404
 from models import Question, Choice
-from django.http import HttpResponse,Http404
+from django.http import HttpResponse,HttpResponseRedirect,Http404
+from django.core.urlresolvers import reverse
 
 # Create your views here.
 
@@ -25,4 +26,15 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("you are voting on question %s."%question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
