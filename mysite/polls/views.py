d245bc476330335fe6a1cee3bacc71ca8cc7ecc6
vyshu020 <vyshu020@gmail.com>
2016-01-28 18:20:06 -0600

    added results.html

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -22,8 +22,8 @@ def detail(request, question_id):
     return render(request,'polls/details.html',context)
 
 def results(request, question_id):
-    response="You are looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request, 'polls/results.html', {'question': question}),
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
