bfed23cad6f4c81f482eb58f9f5fce53c2537911
vyshu020 <vyshu020@gmail.com>
2016-02-01 19:40:32 -0600

    added generic views for index and details

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,8 +6,8 @@ app_name = 'polls'
 
 urlpatterns = [
     url(r'^about_us', views.about_us,),
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<question_id>[0-9]+)/$',views.detail, name='detail'),
+    url(r'^$', views.IndexView.as_view, name='index'),
+    url(r'^(?P<pk><question_id>[0-9]+)/$',views.DetailView.as_view(), name='detail'),
     url(r'^(?P<question_id>[0-9]+)/results/$',views.results, name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 
