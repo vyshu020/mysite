20dbeb8730d0f2dffe579243ba67fdd2a003522c
vyshu020 <vyshu020@gmail.com>
2016-02-02 18:50:30 -0600

    corrected urls.py file

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -7,8 +7,8 @@ app_name = 'polls'
 urlpatterns = [
     url(r'^about_us', views.about_us,),
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<pk><question_id>[0-9]+)/$',views.DetailView.as_view(), name='detail'),
-    url(r'^(?P<question_id>[0-9]+)/results/$',views.ResultsView.as_view(), name='results'),
+    url(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$',views.ResultsView.as_view(), name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 
 ]
