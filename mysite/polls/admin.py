b661a408534656fbe9bb6c5338e2d2f97e12a42d
vyshu020 <vyshu020@gmail.com>
2016-02-15 18:51:08 -0600

    changing field sets

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -4,7 +4,10 @@ from .models import Question, Choice
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ["pub_date",'question_text']
+    fields = [
+        (None, {'fields':['question_text']}),
+        ('Date Information',{'fields':['pub_date']}),
+    ]
 
 admin.site.register(Question,QuestionAdmin)
 admin.site.register(Choice)
