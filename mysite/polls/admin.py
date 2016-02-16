1d431087c6f051a398d5d851221a8590fdd7be3c
vyshu020 <vyshu020@gmail.com>
2016-02-15 19:17:18 -0600

    changed tabular choice form and display of question fields on admin page

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text','pub_date','was_published_recently')
     fieldsets = [
         (None, {'fields':['question_text']}),
         ('Date Information',{'fields':['pub_date']}),
