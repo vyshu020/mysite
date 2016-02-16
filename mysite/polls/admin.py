5dcabe3257261f96e53d283b620dfa6cd8e1355c
vyshu020 <vyshu020@gmail.com>
2016-02-15 19:07:56 -0600

    added choice admin class in admin.py

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,12 +2,16 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields':['question_text']}),
         ('Date Information',{'fields':['pub_date']}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question,QuestionAdmin)
-admin.site.register(Choice)
+
